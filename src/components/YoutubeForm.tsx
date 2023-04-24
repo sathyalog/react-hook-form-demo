21ee187fb80e805abeafd030b61f107ad2d7441c
Sathya Vakacharla <sathyavakacharla@192.168.1.101>
2023-04-24 16:26:50 +0100

    form submission added

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -3,15 +3,24 @@ import { useForm } from "react-hook-form";
 import { DevTool } from "@hookform/devtools";
 
 let renderCount = 0;
+
+type FormValues = {
+  username: string;
+  email: string;
+  channel: string;
+};
 function YoutubeForm() {
-  const form = useForm();
-  const { register, control } = form;
+  const form = useForm<FormValues>();
+  const { register, control, handleSubmit } = form;
   renderCount++;
+  const formSubmission = (data: FormValues) => {
+    console.log("Form submission", data);
+  };
   return (
     <div>
       <h1>YouTube Form({renderCount / 2})</h1>
       {/* renderCount / 2 is used here as react does 2 times render in dev mode */}
-      <form>
+      <form onSubmit={handleSubmit(formSubmission)}>
         <label htmlFor="username">Username</label>
         <input type="text" id="username" {...register("username")} />
 
