c8c1da9ac80d9fb5c7a2c95cf3031c988c26872e
Sathya Vakacharla <sathyavakacharla@192.168.1.101>
2023-04-24 14:55:12 +0100

    managing form state, devtools visualization

---
+++ README.md
@@ -0,0 +1,7 @@
+# React Hook Form demo
+
+A simple demo of the React Hook Form.
+
+Used @hookform/devtools in order to visualize the form elements and its properties.
+
+Using of react hook form helps you to avoid multiple re-renders while using controlled form elements(which happens in React). To visualize it, we are showing renderCount on home page.
--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -1,23 +1,28 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
 
+let renderCount = 0;
 function YoutubeForm() {
   const form = useForm();
+  const { register, control } = form;
+  renderCount++;
   return (
     <div>
-      <h1>YouTube Form</h1>
-
+      <h1>YouTube Form({renderCount / 2})</h1>
+      {/* renderCount / 2 is used here as react does 2 times render in dev mode */}
       <form>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" name="username" />
+        <input type="text" id="username" {...register("username")} />
 
         <label htmlFor="email">E-mail</label>
-        <input type="email" id="email" name="email" />
+        <input type="email" id="email" {...register("email")} />
 
         <label htmlFor="channel">Channel</label>
-        <input type="text" id="channel" name="channel" />
+        <input type="text" id="channel" {...register("channel")} />
 
         <button>Submit</button>
+        <DevTool control={control} />
       </form>
     </div>
   );
