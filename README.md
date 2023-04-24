4454dd7da547bea71dc39689d955afa4e75139c2
Sathya Vakacharla <sathyavakacharla@192.168.1.101>
2023-04-24 16:45:12 +0100

    form validation

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ A simple demo of the React Hook Form.
 Used @hookform/devtools in order to visualize the form elements and its properties.
 
 Using of react hook form helps you to avoid multiple re-renders while using controlled form elements(which happens in React). To visualize it, we are showing renderCount on home page.
+
+Pls follow commits to notice the code changes for each adding feature
+
+As part of this application, we will see that
+
+1. How can we manage form data
+2. Submit form data
+3. Enforce validations and provide visual feedback
--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -20,15 +20,37 @@ function YoutubeForm() {
     <div>
       <h1>YouTube Form({renderCount / 2})</h1>
       {/* renderCount / 2 is used here as react does 2 times render in dev mode */}
-      <form onSubmit={handleSubmit(formSubmission)}>
+      <form onSubmit={handleSubmit(formSubmission)} noValidate>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" {...register("username")} />
+        <input
+          type="text"
+          id="username"
+          {...register("username", {
+            required: "Username is required",
+          })}
+        />
 
         <label htmlFor="email">E-mail</label>
-        <input type="email" id="email" {...register("email")} />
+        <input
+          type="email"
+          id="email"
+          {...register("email", {
+            pattern: {
+              value:
+                /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+              message: "Invalid email format",
+            },
+          })}
+        />
 
         <label htmlFor="channel">Channel</label>
-        <input type="text" id="channel" {...register("channel")} />
+        <input
+          type="text"
+          id="channel"
+          {...register("channel", {
+            required: "Channel is required",
+          })}
+        />
 
         <button>Submit</button>
         <DevTool control={control} />
