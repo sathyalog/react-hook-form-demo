e76b8babd004aea8efcd833c9f34025c4c45f64a
Sathya Vakacharla <sathyavakacharla@192.168.1.101>
2023-04-28 12:59:42 +0100

    get values usage added

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -52,7 +52,7 @@ function YoutubeForm() {
     },
   });
 
-  const { register, control, handleSubmit, formState, watch } = form;
+  const { register, control, handleSubmit, formState, watch, getValues } = form;
   const { errors } = formState;
   const { fields, append, remove } = useFieldArray({
     name: "phNumbers",
@@ -65,23 +65,28 @@ function YoutubeForm() {
     console.log("Form submission", data);
   };
 
-  const watchUsername = watch(["username", "email"]); // watch("username") also works
+  const handleGetValues = () => {
+    console.log("get values", getValues()); // getValues("username") or getValues(["username","email"]) also works
+  };
+
+  /* const watchUsername = watch(["username", "email"]); // watch("username") also works
   const watchForm = watch();
-  console.log(watchUsername);
+  console.log(watchUsername);*/
 
   /*If you want to perform side affect after watching value then use useEffect and watch callback*/
-  useEffect(() => {
+  /*useEffect(() => {
     const subscription = watch((value) => {
       console.log(value); // this console logs every time you type but renders happens once for each field
     });
     return () => subscription.unsubscribe();
   }, [watch]);
+  */
 
   return (
     <div className="form-control">
       <h1>YouTube Form({renderCount / 2})</h1>
-      <h3>Watched values: {watchUsername}</h3>
-      <h4>Watch form: {JSON.stringify(watchForm)}</h4>
+      {/* <h3>Watched values: {watchUsername}</h3>
+      <h4>Watch form: {JSON.stringify(watchForm)}</h4> */}
       {/* renderCount / 2 is used here as react does 2 times render in dev mode */}
       <form onSubmit={handleSubmit(formSubmission)} noValidate>
         <div className="form-control">
@@ -237,6 +242,9 @@ function YoutubeForm() {
             </button>
           </div>
         </div>
+        <button type="button" onClick={handleGetValues}>
+          Get Values
+        </button>
         <button>Submit</button>
       </form>
       <DevTool control={control} />
